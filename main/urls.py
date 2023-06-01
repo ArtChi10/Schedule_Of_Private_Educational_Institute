e8909fc1bb8899c3ca09ce43847cee32551386db
Artem Chibisov <artchibisov10@gmail.com>
2023-06-01 22:39:42 +0400

    Создана веб-страница правки основных сведений

--- main/urls.py
+++ main/urls.py
@@ -3,10 +3,12 @@ from .views import index, other_page
 from .views import ScheduleLoginView
 from .views import profile
 from .views import ScheduleLogoutView
+from .views import ChangeUserInfoView
 
 app_name = 'main'
 urlpatterns = [
     path('accounts/logout/', ScheduleLogoutView.as_view(), name='logout'),
+    path('accounts/profile/change/', ChangeUserInfoView.as_view(), name='profile_change'),
     path('accounts/profile/', profile, name='profile'),
     path('accounts/login/', ScheduleLoginView.as_view(), name='login'),
     path('<str:page>/', other_page, name='other'),
